3faad8841244d6a6da3ca41f69871f74dddf7170
wgb-code <front.willian@gmail.com>
2024-11-10 18:01:06 -0300

    Internacionalização.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient()
+    provideHttpClient(),
+    { 
+      provide: LOCALE_ID,
+      useValue: 'pt-BR' 
+    }
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /// <reference types="@angular/localize" />
 
+import '@angular/localize/init';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
